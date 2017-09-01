d7426b1fea2aede2fa4d01e312ba29608466118e
Michael James Abadilla <mjmaix@gmail.com>
2017-09-01 21:56:15 +0800

    handling public assets, separate css from bundle.js

---
+++ js_modules/src/image_viewer.js
@@ -0,0 +1,13 @@
+import big from '../assets/big.jpeg';
+import small from '../assets/small.jpeg';
+import '../styles/image_viewer.css'
+
+const smallImage = document.createElement('img');
+smallImage.src = small;
+
+document.body.appendChild(smallImage);
+
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -1,4 +1,7 @@
-const sum = require('./sum');
+import '../styles/image_viewer.css';
+
+import sum from './sum';
+import './image_viewer';
 
 const total = sum(10, 5);
 console.log(total);
